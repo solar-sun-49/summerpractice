b4b7998f22530d6214bc2eadb14c328503db3b63
solar-sun-49 <choudhariatharva007@gmail.com>
2026-05-24 20:35:31 +0530

    day4

--- waterbucket.cpp
+++ waterbucket.cpp
@@ -12,3 +12,15 @@ void waterbucket(int a, int b, int c) {
         cout << "NO" << endl;
     }
 }   
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        waterbucket(a, b, c);
+    }
+    return 0;
+}   
+
